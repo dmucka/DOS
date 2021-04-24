f3e7cb5132dc1ceddeb0fb2726f71048d01fb265
Daniel Múčka <daniel.mucka@outlook.sk>
2021-04-24 22:12:34 +0200

    Add project files.

---
+++ DOS_DAL/DOSContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DOS_DAL
+{
+    public class DOSContext : DbContext
+    {
+        private const string _connectionString = "Server=(localdb)\\mssqllocaldb;Integrated Security=True;MultipleActiveResultSets=True;Database=DOSDB;Trusted_Connection=True;";
+
+        public DOSContext(DbContextOptions<DOSContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Seed();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
