3df1faaa98eedeedfb48b1af7fe172c69c5f2ab3
Daniel Múčka <daniel.mucka@outlook.sk>
2021-04-25 02:51:00 +0200

    try syncfusion migration

--- DOS/Startup.cs
+++ DOS/Startup.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Syncfusion.Blazor;
 
 namespace DOS
 {
@@ -26,6 +27,7 @@ namespace DOS
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+			services.AddSyncfusionBlazor();
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
