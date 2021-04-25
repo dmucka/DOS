6c50b9e94b4ff7fd012fc80efa36900c7c0f4b5a
Daniel Múčka <daniel.mucka@outlook.sk>
2021-04-25 22:38:40 +0200

    dal: set default seeding

--- DOS_DAL/DataInitializer.cs
+++ DOS_DAL/DataInitializer.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DOS_DAL.Hashing;
+using DOS_DAL.Models;
+using DOS_DAL.Constants;
 
 namespace DOS_DAL
 {
@@ -11,7 +14,17 @@ namespace DOS_DAL
     {
         internal static void Seed(this ModelBuilder modelBuilder)
         {
+            var adminRole = new Role(RoleConstants.Admin);
+            var operatorRole = new Role(RoleConstants.Operator);
 
+            // id property is needed for the scaffolder
+            adminRole.Id = 1;
+            operatorRole.Id = 2;
+
+            modelBuilder.Entity<Role>().HasData(adminRole, operatorRole);
+
+            // use anonymous class so we can do not need to expose the RoleId property
+            modelBuilder.Entity<User>().HasData(new { Id = 1, Username = "admin", Password = PasswordHasher.Hash("admin"), RoleId = adminRole.Id });
         }
     }
 }
---
+++ DOS_DAL/Migrations/20210425202616_Seed.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DOS_DAL.Migrations
+{
+    public partial class Seed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "dbo",
+                table: "Role",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "Admin" });
+
+            migrationBuilder.InsertData(
+                schema: "dbo",
+                table: "Role",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "Operator" });
+
+            migrationBuilder.InsertData(
+                schema: "dbo",
+                table: "User",
+                columns: new[] { "Id", "Email", "FirstName", "LastName", "Password", "RoleId", "Username" },
+                values: new object[] { 1, null, null, null, "3pHdsgD00IHqAziJbX77CQ==.xuk+8VD+coAy0kimocQX0L1XtAxFHAbnwLT8oi1n0cY=", 1, "admin" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "dbo",
+                table: "Role",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                schema: "dbo",
+                table: "User",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                schema: "dbo",
+                table: "Role",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
