4c3d99b8eb7c13c72046943e549dab26bdb02859
Daniel Múčka <daniel.mucka@outlook.sk>
2021-04-25 22:36:40 +0200

    dal: implement hashing

---
+++ DOS_DAL/Hashing/PasswordHasher.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOS_DAL.Hashing
+{
+    // https://medium.com/dealeron-dev/storing-passwords-in-net-core-3de29a3da4d2
+    public static class PasswordHasher
+    {
+        private const int SaltSize = 16; // 128 bit 
+        private const int KeySize = 32; // 256 bit
+        private const int Iterations = 1000; // 1ms
+
+        public static string Hash(string password)
+        {
+            using (var algorithm = new Rfc2898DeriveBytes(
+              password,
+              SaltSize,
+              Iterations,
+              HashAlgorithmName.SHA256))
+            {
+                var key = Convert.ToBase64String(algorithm.GetBytes(KeySize));
+                var salt = Convert.ToBase64String(algorithm.Salt);
+
+                return $"{salt}.{key}";
+            }
+        }
+
+        public static bool Check(string hash, string password)
+        {
+            var parts = hash.Split('.', 2);
+
+            if (parts.Length != 2)
+            {
+                throw new FormatException("Unexpected hash format.");
+            }
+
+            var salt = Convert.FromBase64String(parts[0]);
+            var key = Convert.FromBase64String(parts[1]);
+
+            using (var algorithm = new Rfc2898DeriveBytes(
+              password,
+              salt,
+              Iterations,
+              HashAlgorithmName.SHA256))
+            {
+                var keyToCheck = algorithm.GetBytes(KeySize);
+                return keyToCheck.SequenceEqual(key);
+            }
+        }
+    }
+}
