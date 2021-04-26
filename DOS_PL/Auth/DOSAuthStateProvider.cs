face8efaf3c45b929fb8fcd6d83afda27e751a27
Daniel Múčka <daniel.mucka@outlook.sk>
2021-04-26 03:47:12 +0200

    pl: login page init

---
+++ DOS_PL/Auth/DOSAuthStateProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace DOS_PL.Auth
+{
+    public class DOSAuthStateProvider : AuthenticationStateProvider
+    {
+        public override Task<AuthenticationState> GetAuthenticationStateAsync() => throw new NotImplementedException();
+    }
+}
