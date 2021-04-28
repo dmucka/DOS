060cb4b2b7a9d1d4678e3aa87e2e581e1a216eae
Daniel Múčka <daniel.mucka@outlook.sk>
2021-04-29 00:38:39 +0200

    bl: userservice: add authentication method

--- DOS_BL/Services/UserService.cs
+++ DOS_BL/Services/UserService.cs
@@ -1,5 +1,7 @@
 ﻿using DOS_DAL;
 using DOS_DAL.Models;
+using DOS_DAL.Hashing;
+using System.Linq;
 
 namespace DOS_BL.Services
 {
@@ -7,6 +9,15 @@ namespace DOS_BL.Services
     {
         public UserService(DOSContext dbcontext) : base(dbcontext)
         {
+        }
+
+        public (bool Success, User user) AuthenticateUser(string username, string password)
+        {
+            var foundUser = AsQueryable().FirstOrDefault(user => user.Username == username);
+            if (foundUser is null)
+                return (false, null);
+
+            return (PasswordHasher.Check(foundUser.Password, password), foundUser);
         }
     }
 }
