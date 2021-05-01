457bb9e3a3ac1f5300ef914f8cb2af6425ca0d62
Daniel Múčka <daniel.mucka@outlook.sk>
2021-05-01 16:18:57 +0200

    pl: validator for optional fields

---
+++ DOS_PL/Validation/OptionalFieldValidator.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DOS_PL.Validation
+{
+    public class OptionalFieldValidator : ComponentBase
+    {
+        [CascadingParameter]
+        EditContext CurrentEditContext { get; set; }
+
+        /// <inheritdoc />
+        protected override void OnInitialized()
+        {
+            if (CurrentEditContext == null)
+            {
+                throw new InvalidOperationException(
+                    $"{nameof(OptionalFieldValidator)} requires a cascading " +
+                    $"parameter of type {nameof(EditContext)}. " +
+                    $"For example, you can use {nameof(OptionalFieldValidator)} " +
+                    $"inside an {nameof(EditForm)}.");
+            }
+
+            CurrentEditContext.OnFieldChanged += (s, e) =>
+            {
+                var prop = e.FieldIdentifier.Model.GetType().GetProperty(e.FieldIdentifier.FieldName);
+                if (prop == null || prop.PropertyType != typeof(string))
+                    return;
+
+                // continue only if the property does not have the Required attribute
+                if (prop.GetCustomAttributes(typeof(RequiredAttribute), false).Length != 0)
+                    return;
+
+                // check if the string is empty
+                if (prop.GetValue(e.FieldIdentifier.Model) as string == "")
+                {
+                    prop.SetValue(e.FieldIdentifier.Model, null);
+                    CurrentEditContext.MarkAsUnmodified(e.FieldIdentifier);
+                    CurrentEditContext.Validate();
+                    StateHasChanged();
+                }
+            };
+        }
+    }
+}
