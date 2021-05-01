af6a18ee28b285e2bf552b697931cd0fa2aea6d4
Daniel Múčka <daniel.mucka@outlook.sk>
2021-05-01 16:21:22 +0200

    pl: users: add

--- DOS_BL/Services/UserService.cs
+++ DOS_BL/Services/UserService.cs
@@ -2,6 +2,7 @@
 using DOS_DAL.Models;
 using DOS_DAL.Hashing;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace DOS_BL.Services
 {
@@ -11,13 +12,23 @@ namespace DOS_BL.Services
         {
         }
 
+        public User GetByName(string username)
+        {
+            return AsQueryable().FirstOrDefault(user => user.Username == username);
+        }
+
         public (bool Success, User User) AuthenticateUser(string username, string password)
         {
-            var foundUser = AsQueryable().FirstOrDefault(user => user.Username == username);
+            var foundUser = GetByName(username);
             if (foundUser is null)
                 return (false, null);
 
             return (PasswordHasher.Check(foundUser.Password, password), foundUser);
+        }
+
+        public Task<bool> CreateNewUser(User user)
+        {
+            //TODO: Add DTO mapping
         }
     }
 }
