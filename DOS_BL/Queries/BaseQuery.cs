9d1b62316273f61717ef397fd38a9748f5c9750a
Daniel Múčka <daniel.mucka@outlook.sk>
2021-05-04 18:28:29 +0200

    bl: query: expose ToListAsync

--- DOS_BL/Queries/BaseQuery.cs
+++ DOS_BL/Queries/BaseQuery.cs
@@ -13,5 +13,8 @@ namespace DOS_BL.Queries
         public static IQueryable<T> OrderById<T>(this IQueryable<T> query) where T : IBaseModel
             => query.OrderBy(x => x.Id)
                     .AsQueryable();
+
+        public static Task<List<T>> ToListAsync<T>(this IQueryable<T> query) where T : IBaseModel
+            => EntityFrameworkQueryableExtensions.ToListAsync(query);
     }
 }
